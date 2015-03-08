98d8f657dc05b2f2631e56e633835502caef76b3
DigitalDreamr <james.brian.odonnell@gmail.com>
2015-03-09 02:59:04 +1100

    Complete Project 1 Commit
    Complete Course Project 1 for Exploratory Data Analysis on Coursera

---
+++ plot1.R
@@ -0,0 +1,10 @@
+### plot1.R
+if (!exists("getandprep")) source("getandprep.R")
+getandprep()
+
+png(filename="plot1.png",width=480,height=480)
+with(hpc,hist(kWact,12,col="red"
+     ,main="Global Active Power"
+     ,xlab="Global Active Power (kilowatts)"
+     ,ylab="Frequency"))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,13 @@
+### plot3.R 
+if (!exists("getandprep")) source("getandprep.R")
+getandprep()
+
+png(filename="plot3.png",width=480,height=480)
+with(hpc,plot(datetime,Wh1,type="l"
+     ,xlab="",ylab="Energy sub metering"))
+with(hpc,lines(datetime,Wh2,col="red"))
+with(hpc,lines(datetime,Wh3,col="blue"))
+with(hpc,legend("topright",lty=c(1,1,1)
+     ,col=c("black","red","blue")
+     ,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")))
+dev.off()
